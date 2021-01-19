d3a5952b0335e1c4cde86b269ca31e75ac41ba9f
Anthony Bui <76550986+anthonybui94@users.noreply.github.com>
2021-01-18 20:50:50 -0600

    Added delete task functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,29 +5,43 @@ class TodoList extends Component {
     super(props);
     this.state = { 
       info: [
-        {id: 1, name: "powerlift"}, 
-        {id: 2, name: "meditate"},
-        {id: 3, name: "groceries"},
-        {id: 4, name: "laundry"},
-        {id: 5, name: "study react"}
-      ]
+        {id: 1, name: "meditate"},
+        {id: 2, name: "groceries"},
+        {id: 3, name: "laundry"},
+        {id: 4, name: "study react"}
+      ],
+      newTask: ''
      }
     this.handleDoubleClick = this.handleDoubleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleDoubleClick(event) {
     event.preventDefault();
-    console.log(event.target.id)
-    console.log(this.state.info)
     this.setState({
       // item.id is a number but event.target.id is a string... LOL
       info: this.state.info.filter(item => item.id.toString() !== event.target.id)
     })
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      info: [{ id: Math.floor(Math.random()*10000000), name: this.state.newTask }, ...this.state.info],
+      newTask: ''
+    })
+  }
 
+  handleChange(event) {
+    this.setState({
+      newTask: event.target.value
+    })
+  }
 
   render() { 
+    const newTask = this.state.newTask
+
     return ( 
       <div className="list-info">
         <ul>
@@ -40,8 +54,14 @@ class TodoList extends Component {
             </li>)
           })}
         </ul>
+        <input type="text" 
+        value={newTask} 
+        placeholder="Enter task..."
+        onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Add</button>
       </div>
-     );
+    );
   }
 }
  
